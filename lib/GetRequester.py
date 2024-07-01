a4b29bf4bb49cf48608e2d083fe77e04da1594c0
Laurine Maina <mainalaureen58@gmail.com>
2024-07-01 09:22:29 +0300

    commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -13,3 +13,10 @@ class GetRequester:
     def load_json(self):
         response_body = self.get_response_body()
         return json.loads(response_body)
+    
+# get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+# get_requester.load_json()
+
+# requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+# print(requester.get_response_body())
+# print(requester.load_json())
